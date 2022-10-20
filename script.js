742f32ed132c2b4737ecf7fb9e547c69491277a4
Dream1ess <lagiar1703@gmail.com>
2022-10-20 03:30:33 +0300

    added change contrast

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
-const getSkills = () => document.querySelectorAll('.resume-skill');
-const setPercent = array => {
-    for(let item of array) {
-        const percentBlock = item.querySelector('.resume-skill__percent'),
-            levelBlock = item.querySelector('.resume-skill__level span');
-        let percent = '0';
-        if(percentBlock) {
-            percent = percentBlock.innerText;
+page = {};
+page.main = {
+    data: {
+        btnContrast: document.getElementById('contrast')
+    },
+    handlers: {
+        changeContrast: () => {
+            if(page.main.data.btnContrast.classList.contains('active')) {
+                page.main.data.btnContrast.classList.remove('active');
+                for(let item of page.main.methods.getContrastElement()) {
+                    item.classList.remove('active');
+                }
+            } else {
+                page.main.data.btnContrast.classList.add('active');
+                for(let item of page.main.methods.getContrastElement()) {
+                    item.classList.add('active');
+                }
+            }
         }
-        levelBlock.style.width = percent;
+    },
+    methods: {
+        getContrastElement: () => document.querySelectorAll('.contrast'),
+        getSkills: () => document.querySelectorAll('.resume-skill'),
+        setPercent: array => {
+            for(let item of array) {
+                const percentBlock = item.querySelector('.resume-skill__percent'),
+                    levelBlock = item.querySelector('.resume-skill__level span');
+                let percent = '0';
+                if(percentBlock) {
+                    percent = percentBlock.innerText;
+                }
+                levelBlock.style.width = percent;
+            }
+        }
     }
-}
-
-setPercent(getSkills());
+};
+(function (){
+    page.main.methods.setPercent(page.main.methods.getSkills());
+    page.main.data.btnContrast.addEventListener('click', page.main.handlers.changeContrast)
+}());
