ee53743d2c0b011bd1bbaba8ab0513506762c7ed
Dream1ess <lagiar1703@gmail.com>
2022-10-20 15:42:37 +0300

    sw

--- sw.js
+++ sw.js
@@ -25,3 +25,19 @@ self.addEventListener('install', (event) => {
     })
   );
 });
+self.addEventListener('activate', (event) => {
+    console.log('activate');
+});
+self.addEventListener('fetch', function(event) {
+   event.respondWith(async function() {
+      try{
+        let res = await fetch(event.request);
+        let cache = await caches.open('cache');
+        await cache.put(event.request.url, res.clone());
+        return res;
+      }
+      catch(error){
+        return caches.match(event.request);
+       }
+     }());
+ });
